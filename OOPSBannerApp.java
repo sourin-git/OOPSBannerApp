db6931060742a7f43e97b47cc030db2699776ba1
sourin-git <sourin.csa@gmail.com>
2026-02-16 09:12:56 +0530

    Coded to Print OOPS to Console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * UC1: Print OOPS to Console
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // Display the literal text OOPS
+        System.out.println("OOPS");
+    }
+}
