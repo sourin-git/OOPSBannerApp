b3d7597aba11e55bf4f0d8d6825ca1dbefc604de
sourin-git <sourin.csa@gmail.com>
2026-02-17 09:43:35 +0530

    Coded to Render OOPS as Banner using Print Statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,18 @@
+/**
+ * UC2: Print OOPS Banner to Console
+ * Displays "OOPS" using ASCII art stars.
+ * @author Sourin
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println("  *****     *****   ********   ******* ");
+        System.out.println(" **   **   **   **  **     **  **     **");
+        System.out.println(" **   **   **   **  **     **  **");
+        System.out.println(" **   **   **   **  ********   ******* ");
+        System.out.println(" **   **   **   **  **         **     **");
+        System.out.println(" **   **   **   **  **         **     **");
+        System.out.println("  *****     *****   **          ******* ");
+    }
+}
